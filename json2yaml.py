9c541453129ff6eb7ba8f4ea94d93d87b4c3ec35
AnShen <shen.an89@gmail.com>
2021-05-24 12:50:41 +0800

    update code

--- json2yaml.py
+++ json2yaml.py
@@ -3,15 +3,18 @@ import json
 import yaml
 import sys
 
-json_file = sys.argv[1]
+def main():
+    json_file = sys.argv[1]
 
-file_name = json_file.split('.')[0]
+    file_name = json_file.split('.')[0]
 
-yaml_file = file_name + '.yaml'
+    yaml_file = file_name + '.yaml'
 
-with open(json_file, 'r') as f:
-    data = json.loads(f.read())
+    with open(json_file, 'r') as f:
+        data = json.loads(f.read())
 
-with open(yaml_file, 'w') as f:
-    yaml.dump(data, f, default_flow_style=False, encoding='utf-8',  allow_unicode=True)
+    with open(yaml_file, 'w') as f:
+        yaml.dump(data, f, default_flow_style=False, encoding='utf-8',  allow_unicode=True)
 
+if __name__ == '__main__':
+    main()
